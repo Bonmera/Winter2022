d3a652ead0fd33075f4edd54f5438e75d8646fae
Chaitanya Subhedar <csubhedar1996@gmail.com>
2022-02-09 03:10:00 -0600

    added camera controller attached to player

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -12,6 +12,8 @@ public class CharacterController2D : MonoBehaviour
     private float k_HorizontalSpeed = 0f;
     private float k_VerticalSpeed = 0f;
     private Vector3 m_CurrVelocity = Vector3.zero;
+    private Vector2 screenBounds;
+    private Vector2 playerOffset;
 
     private void Awake()
     {
@@ -20,7 +22,9 @@ public class CharacterController2D : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        playerOffset.x = transform.GetComponent<SpriteRenderer>().bounds.size.x/2;
+        playerOffset.y = transform.GetComponent<SpriteRenderer>().bounds.size.y/2;
     }
 
     // Update is called once per frame
@@ -44,5 +48,10 @@ public class CharacterController2D : MonoBehaviour
     {
         Vector3 targetVelocity = new Vector2(k_HorizontalSpeed , k_VerticalSpeed );
         m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_CurrVelocity, m_MovementSmoothing);
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, Camera.main.transform.position.x-1*screenBounds.x + playerOffset.x , Camera.main.transform.position.x+ screenBounds.x - playerOffset.x ), Mathf.Clamp(transform.position.y, Camera.main.transform.position.y-screenBounds.y + playerOffset.y , Camera.main.transform.position.y + screenBounds.y - playerOffset.y ), transform.position.z);
     }
 }
--- Assets/Scripts/cameraMovement.cs
+++ Assets/Scripts/cameraMovement.cs
@@ -6,6 +6,10 @@ public class cameraMovement : MonoBehaviour
 {
     [SerializeField]
     BoxCollider2D mapBounds;
+    [SerializeField]
+    GameObject targetObject;
+    [SerializeField]
+    float trackingSpeed = 1;
 
     private float xMin, xMax, yMin, yMax;
     private Camera cam;
@@ -13,8 +17,8 @@ public class cameraMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 offset = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         cam = GetComponent<Camera>();
+        Vector3 offset = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, cam.transform.position.z));
         xMin = mapBounds.bounds.min.x + offset.x;
         xMax = mapBounds.bounds.max.x - offset.x;
         yMin = mapBounds.bounds.min.y + offset.y;
@@ -24,8 +28,9 @@ public class cameraMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 mousePos = Input.mousePosition;
-        Vector3 point = cam.ScreenToWorldPoint(new Vector2(mousePos.x, mousePos.y));
-        cam.transform.position = new Vector3(Mathf.Lerp(cam.transform.position.x, Mathf.Clamp(point.x, xMin, xMax),Time.deltaTime),Mathf.Lerp(cam.transform.position.y, Mathf.Clamp(point.y, yMin, yMax),Time.deltaTime),cam.transform.position.z);
+        Vector3 point = targetObject.transform.position;
+        //Debug.Log(yMin+"  "+yMax);
+        //Debug.Log(cam.transform.position);
+        cam.transform.position = new Vector3(Mathf.Lerp(cam.transform.position.x, Mathf.Clamp(point.x, xMin, xMax),Time.deltaTime*trackingSpeed),Mathf.Lerp(cam.transform.position.y, Mathf.Clamp(point.y, yMin, yMax),Time.deltaTime*trackingSpeed),cam.transform.position.z);
     }
 }
